21fcb21583c48f8ad4501ae19176fd8ff774192b
Neco Arc <necoarc001@icloud.com>
2024-10-23 11:09:21 +0200

    Update lineage_TB125FU.mk

--- lineage_TB125FU.mk
+++ lineage_TB125FU.mk
@@ -6,10 +6,9 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
 # Inherit from TB125FU device
 $(call inherit-product, device/lenovo/TB125FU/device.mk)
