4f81f109fe0854b47b03ae150a923ebf4ee49c2b
Neco Arc <necoarc001@icloud.com>
2024-12-25 14:18:31 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -78,16 +78,18 @@ TARGET_KERNEL_SOURCE := kernel/lenovo/TB125FU
 #Kernel modules
 TARGET_KERNEL_EXT_MODULE_ROOT := kernel/lenovo/TB125FU-modules
 TARGET_KERNEL_EXT_MODULES := \
-    kernel_modules/connectivity/conninfra \
-    kernel_modules/connectivity/connfem \
-    kernel_modules/connectivity/fmradio \
-    kernel_modules/connectivity/wlan/adaptor \
-    kernel_modules/connectivity/wlan/core/gen4m \
-    kernel_modules/connectivity/bt/linux_v2 \
-    kernel_modules/connectivity/bt/mt66xx \
-    kernel_modules/connectivity/common \
-    kernel_modules/connectivity/gps \
-    kernel_modules/udc
+	connectivity/conninfra \
+	connectivity/connfem \
+	connectivity/fmradio \
+	connectivity/wlan/adaptor \
+	connectivity/wlan/core/gen4m \
+	connectivity/bt/linux_v2 \
+	connectivity/bt/mt66xx \
+	connectivity/common \
+	connectivity/gps \
+	udc \
+	fpsgo_cus \
+	met_drv_v2 \    
 BOARD_SYSTEM_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))
 BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))
 BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))
