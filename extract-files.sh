2e5256addc1ee93f702616ce3bf66da2bf1764b2
Neco Arc <necoarc001@icloud.com>
2025-03-24 11:02:11 +0100

    Update extract-files.sh

--- extract-files.sh
+++ extract-files.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 #
-# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
-# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2023 The LineageOS Project
+#
 # SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
 
 DEVICE=TB125FU
-VENDOR=LENOVO
+VENDOR=lenovo
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
 ANDROID_ROOT="${MY_DIR}/../../.."
-
-# If XML files don't have comments before the XML header, use this flag
-# Can still be used with broken XML files by using blob_fixup
-export TARGET_DISABLE_XML_FIXING=true
 
 HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
@@ -33,27 +30,25 @@ CLEAN_VENDOR=true
 ONLY_FIRMWARE=
 KANG=
 SECTION=
-CARRIER_SKIP_FILES=()
 
 while [ "${#}" -gt 0 ]; do
     case "${1}" in
-        --only-firmware)
-            ONLY_FIRMWARE=true
-            ;;
-        -n | --no-cleanup)
-            CLEAN_VENDOR=false
-            ;;
-        -k | --kang)
-            KANG="--kang"
-            ;;
-        -s | --section)
-            SECTION="${2}"
-            shift
-            CLEAN_VENDOR=false
-            ;;
-        *)
-            SRC="${1}"
-            ;;
+        --only-firmware )
+                ONLY_FIRMWARE=true
+                ;;
+        -n | --no-cleanup )
+                CLEAN_VENDOR=false
+                ;;
+        -k | --kang )
+                KANG="--kang"
+                ;;
+        -s | --section )
+                SECTION="${2}"; shift
+                CLEAN_VENDOR=false
+                ;;
+        * )
+                SRC="${1}"
+                ;;
     esac
     shift
 done
@@ -62,8 +57,8 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
-function blob_fixup() {
-    case "${1}" in
+function blob_fixup {
+    case "$1" in
         system_ext/lib64/libsink.so)
             "${PATCHELF}" --add-needed "libshim_sink.so" "$2"
             ;;
@@ -104,24 +99,6 @@ function blob_fixup() {
             "$PATCHELF" --replace-needed "libcamera2ndk_vendor.so" "libcamera2ndk_vendor-v35.so" "$2"
             ;;
     esac
-
-    return 0
-}
-
-function blob_fixup_dry() {
-    blob_fixup "$1" ""
-}
-
-function prepare_firmware() {
-    if [ "${SRC}" != "adb" ]; then
-        local STAR="${ANDROID_ROOT}"/lineage/scripts/motorola/star.sh
-        for IMAGE in bootloader radio; do
-            if [ -f "${SRC}/${IMAGE}.img" ]; then
-                echo "Extracting Motorola star image ${SRC}/${IMAGE}.img"
-                sh "${STAR}" "${SRC}/${IMAGE}.img" "${SRC}"
-            fi
-        done
-    fi
 }
 
 # Initialize the helper
@@ -129,11 +106,6 @@ setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
 if [ -z "${ONLY_FIRMWARE}" ]; then
     extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
-
-    generate_prop_list_from_image "product.img" "${MY_DIR}/proprietary-files-carriersettings.txt" CARRIER_SKIP_FILES carriersettings
-    extract "${MY_DIR}/proprietary-files-carriersettings.txt" "${SRC}" "${KANG}" --section "${SECTION}"
-
-    extract_carriersettings
 fi
 
 if [ -z "${SECTION}" ]; then
